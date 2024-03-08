daa5854487a92b596c6e692ee58d7fa543711b80
manuelmsni <manuelmsni@hotmail.com>
2024-03-08 21:38:55 +0100

    avanzado

--- acceso_a_datos/ApiRest/api/src/main/java/com/mycompany/api/util/Constants.java
+++ acceso_a_datos/ApiRest/api/src/main/java/com/mycompany/api/util/Constants.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.api.persistence;
+package com.mycompany.api.util;
 
 /**
  *
  * @author manuelmsni
  */
-public class HibernatePersistence {
-    
+public class Constants {
+    public static final String CONFIG_FILE = "app.conf";
 }
--- acceso_a_datos/ApiRest/api/src/main/java/com/mycompany/api/util/JWTManager.java
+++ acceso_a_datos/ApiRest/api/src/main/java/com/mycompany/api/util/JWTManager.java
@@ -15,7 +15,7 @@ import java.util.Date;
 
 /**
  *
- * @author Manuelmsni
+ * @author manuelmsni
  */
 public class JWTManager {
 
